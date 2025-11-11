5863075fb75c63f1cf0fc15db537beb971976e49
funizubes <funizubes@users.noreply.github.com>
2025-11-11 17:14:26 +0000

    Add mock-constants.tsx, mock-gateway-auth.ts

---
+++ mock-constants.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+interface main_order_subscriptionsProps { data: string }
+
---
+++ mock-gateway-auth.ts
@@ -0,0 +1,4 @@
+import { ethers } from 'ethers';
+
+interface message_utils_messageProps { data: string }
+
